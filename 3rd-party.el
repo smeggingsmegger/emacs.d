7f0a245897c40c2719eda1fd1ca825acc3c814ad
Ben Hayden <ben@britecore.com>
2014-03-17 00:20:04 -0500

    Remove unused package.

--- 3rd-party.el
+++ 3rd-party.el
@@ -1,9 +1,6 @@
 ;; On the fly syntax checking
 (require-package 'flycheck)
-(require-package 'flycheck-color-mode-line)
 (global-flycheck-mode 1)
-;; Looks bad now, but would be nice to add back in
-;; (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)
 
 ;; Git Stuffis
 (require-package 'magit)
