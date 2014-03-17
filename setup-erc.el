145ac0740936b2e79eb25255e385f3c738f46914
Ben Hayden <ben@britecore.com>
2014-03-17 09:54:47 -0500

    Ignore some tracking in ERC.

--- setup-erc.el
+++ setup-erc.el
@@ -1,4 +1,7 @@
 ;; Set nicknames to be highlighted
 (setq erc-keywords '("ben" "Ben" "beardedprojamz" "everyone" "guys"))
 
+;; Ignore some track stuff
+(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT"))
+
 (provide 'setup-erc)
