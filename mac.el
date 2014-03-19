59d743544842e04b0ccf7d679a8cbaef6b2b58bf
Ben Hayden <hayden767@gmail.com>
2014-03-18 22:34:07 -0500

    Setting Mac keys back

--- mac.el
+++ mac.el
@@ -1,3 +1,11 @@
+;; Change command to meta - more like normal keyboard
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+(setq ns-function-modifier 'hyper)
+
+;; keybinding to toggle full screen mode
+(global-set-key (quote [M-f10]) (quote ns-toggle-fullscreen))
+
 ;; Use aspell for spell checking: brew install aspell --lang=en
 (setq ispell-program-name "/usr/local/bin/aspell")
 
